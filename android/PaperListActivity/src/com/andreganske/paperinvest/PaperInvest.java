9547f6b473919438eaeab5fea4bfffd30676d236
Andre Gerson Ganske <andre.ganske@fluig.com>
2014-11-19 11:53:00 -0200

    alteração na chave depois de fazer cagada

--- android/PaperListActivity/src/com/andreganske/paperinvest/PaperInvest.java
+++ android/PaperListActivity/src/com/andreganske/paperinvest/PaperInvest.java
@@ -2,11 +2,6 @@ package com.andreganske.paperinvest;
 
 import android.app.Application;
 
-import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseObject;
-import com.parse.ParseUser;
-
 public class PaperInvest extends Application {
 
     public static final String PAPER_GROUP_NAME = "ALL_PAPERS";
@@ -18,7 +13,7 @@ public class PaperInvest extends Application {
         ParseObject.registerSubclass(Paper.class);
 
         Parse.enableLocalDatastore(getApplicationContext());
-        Parse.initialize(this, "vy6Q3MjXafFT8VGo3KQH0H1IdXMfn8ZaSdEhCqGe", "yIEoIgCfov5BPLPcoK5OdI19uDwsE43bvnkNfoUV");
+        Parse.initialize(this, "P87xbt0SGXOAdjsHk2CiXPugwWL6vzQfNf7QEVcF", "SgINFlm3D5qbnlhEROOQUbuVD6eyhB6C8qf6YG8F");
 
         ParseUser.enableAutomaticUser();
         ParseACL defaultACL = new ParseACL();

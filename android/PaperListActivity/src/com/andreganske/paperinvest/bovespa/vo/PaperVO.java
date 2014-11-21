bcf524a00517e525b5f08770ea4d995399ba32f6
andreganske <andre.g.ganske@gmail.com>
2014-11-20 23:55:41 -0200

    removendo coisas desnecessarias e formatacao

--- android/PaperListActivity/src/com/andreganske/paperinvest/bovespa/vo/PaperVO.java
+++ android/PaperListActivity/src/com/andreganske/paperinvest/bovespa/vo/PaperVO.java
@@ -23,16 +23,16 @@ public class PaperVO {
     private String oscilacao;
 
     public PaperVO() {
-    	super();
+        super();
     }
-    
+
     public PaperVO(String codigo) {
-    	super();
-    	this.codigo = codigo;
+        super();
+        this.codigo = codigo;
     }
 
     public PaperVO(String codigo, String nome, String ibovespa, String data, String abertura, String minimo,
-            String maximo, String medio, String ultimo, String oscilacao, String minino) {
+            String maximo, String medio, String ultimo, String oscilacao) {
         this.codigo = codigo;
         this.nome = nome;
         this.ibovespa = ibovespa;
@@ -43,87 +43,86 @@ public class PaperVO {
         this.medio = medio;
         this.ultimo = ultimo;
         this.oscilacao = oscilacao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIbovespa() {
+        return ibovespa;
+    }
+
+    public void setIbovespa(String ibovespa) {
+        this.ibovespa = ibovespa;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getAbertura() {
+        return abertura;
+    }
+
+    public void setAbertura(String abertura) {
+        this.abertura = abertura;
+    }
+
+    public String getMinimo() {
+        return minimo;
+    }
+
+    public void setMinimo(String minimo) {
         this.minimo = minimo;
     }
 
-	public String getCodigo() {
-		return codigo;
-	}
+    public String getMaximo() {
+        return maximo;
+    }
 
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
+    public void setMaximo(String maximo) {
+        this.maximo = maximo;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getMedio() {
+        return medio;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setMedio(String medio) {
+        this.medio = medio;
+    }
 
-	public String getIbovespa() {
-		return ibovespa;
-	}
+    public String getUltimo() {
+        return ultimo;
+    }
 
-	public void setIbovespa(String ibovespa) {
-		this.ibovespa = ibovespa;
-	}
+    public void setUltimo(String ultimo) {
+        this.ultimo = ultimo;
+    }
 
-	public String getData() {
-		return data;
-	}
+    public String getOscilacao() {
+        return oscilacao;
+    }
 
-	public void setData(String data) {
-		this.data = data;
-	}
-
-	public String getAbertura() {
-		return abertura;
-	}
-
-	public void setAbertura(String abertura) {
-		this.abertura = abertura;
-	}
-
-	public String getMinimo() {
-		return minimo;
-	}
-
-	public void setMinimo(String minimo) {
-		this.minimo = minimo;
-	}
-
-	public String getMaximo() {
-		return maximo;
-	}
-
-	public void setMaximo(String maximo) {
-		this.maximo = maximo;
-	}
-
-	public String getMedio() {
-		return medio;
-	}
-
-	public void setMedio(String medio) {
-		this.medio = medio;
-	}
-
-	public String getUltimo() {
-		return ultimo;
-	}
-
-	public void setUltimo(String ultimo) {
-		this.ultimo = ultimo;
-	}
-
-	public String getOscilacao() {
-		return oscilacao;
-	}
-
-	public void setOscilacao(String oscilacao) {
-		this.oscilacao = oscilacao;
-	}
+    public void setOscilacao(String oscilacao) {
+        this.oscilacao = oscilacao;
+    }
 
 }
